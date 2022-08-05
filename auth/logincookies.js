669d278263229c7033fdfb65460be70159f41d1e
JayellLiburd <103109287+JayellLiburd@users.noreply.github.com>
2022-08-05 17:21:04 -0500

    fixed cookie bug by changing secure attibute

--- auth/logincookies.js
+++ auth/logincookies.js
@@ -89,8 +89,8 @@ router.get('/:id/login', (req, res) => {
                         const tokenpref = sign({dark: response[0].dark, weather: response[0].weather, favorites: response[0].favorites}, 'password')
 
                         res
-                            .cookie('ss', AuthToken, {sameSite: "none", secure: true, httpOnly: true, path: 'queup.vercel.app/'})
-                            .cookie('rs', VToken, {sameSite: "none", secure: true, path: 'queup.vercel.app/'})
+                            .setHeader('set-cookie', ('ss', AuthToken, {sameSite: "none", secure: true, httpOnly: true, path: 'queup.vercel.app/'}))
+                            .setHeader('set-cookie', ('rs', VToken, {sameSite: "none", secure: true, path: 'queup.vercel.app/'}))
                             .send([result[0].first_name, tokenpref])
                         }
 
