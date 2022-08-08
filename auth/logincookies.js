45f20b775a0492bb3c474274fabd7946e9a5d76b
JayellLiburd <103109287+JayellLiburd@users.noreply.github.com>
2022-08-08 01:24:02 -0500

    forgot to change links

--- auth/logincookies.js
+++ auth/logincookies.js
@@ -19,7 +19,7 @@ const db = mysql.createPool({
 
 
 
-// After verify this sends cookie with get usin url form react form //
+// After verifying Login this sends cookie with get usin url form react form //
 router.get('/:id/login', (req, res) => {
 
     var ua = parser(req.headers['user-agent']);
@@ -37,40 +37,39 @@ router.get('/:id/login', (req, res) => {
             if (err) { res.send({err: err}); }
             else {
 
-                //If Username match then...
-                if (result[0]) {  
+            //If Username match then...
+            if (result[0]) {  
 
+                const AuthToken = sign({ ssuid: result[0].user_id }, 'password')
+                const VToken = sign({ ssu: result[0].username }, 'password')
 
-                    const AuthToken = sign({ ssuid: result[0].user_id }, 'password')
-                    const VToken = sign({ ssu: result[0].username }, 'password')
+                //grabs prefrences
+                db.query(findpref, user_id, (err, response) => {
+                    
+                    //Create if dont have
+                    if (response.length <= 0) {db.query(constpref, user_id, (err, results) => {
 
-                    //grabs prefrences
-                    db.query(findpref, user_id, (err, response) => {
-                        
-                        //Create if dont have
-                        if (response.length <= 0) {db.query(constpref, user_id, (err, results) => {
+                        //Now regrab with newly created prefrences and send cookies like normal
+                        db.query(findpref, user_id, (err, response) => {
 
-                            //Now regrab with newly created prefrences and send cookies like normal
-                            db.query(findpref, user_id, (err, response) => {
+                            const tokenpref = sign({dark: response[0].dark, weather: response[0].weather, favorites: response[0].favorites}, 'password')
 
-                                const tokenpref = sign({dark: response[0].dark, weather: response[0].weather, favorites: response[0].favorites}, 'password')
-
-                                if(ua.device.model == 'iPhone'){
-                                    res
-                                    .cookie('ss', AuthToken, {sameSite: "none", secure: true, domain: 'queueupnext.com'})
+                            if(ua.device.model == 'iPhone'){
+                                res
+                                .cookie('ss', AuthToken, {sameSite: "none", secure: true, domain: 'queueupnext.com'})
+                                .cookie('rs', VToken, {sameSite: "none", secure: true, domain: 'queueupnext.com'})
+                                .send([result[0].first_name, tokenpref])
+                            }
+                            else {
+                                res
+                                    .cookie('ss', AuthToken, {sameSite: "none", secure: true, httpOnly: true, domain: 'queueupnext.com'})
                                     .cookie('rs', VToken, {sameSite: "none", secure: true, domain: 'queueupnext.com'})
                                     .send([result[0].first_name, tokenpref])
-                                }
-                                else {
-                                    res
-                                        .cookie('ss', AuthToken, {sameSite: "none", secure: true, httpOnly: true, domain: 'queueupnext.com'})
-                                        .cookie('rs', VToken, {sameSite: "none", secure: true, domain: 'queueupnext.com'})
-                                        .send([result[0].first_name, tokenpref])
-                                }           
-                        })})}
+                            }           
+                    })})}
 
-                        //Grab prefrences if already created
-                        else {
+                    //Grab prefrences if already created
+                    else {
                         const tokenpref = sign({dark: response[0].dark, weather: response[0].weather, favorites: response[0].favorites}, 'password')
 
                         if(ua.device.model == 'iPhone'){
@@ -84,18 +83,17 @@ router.get('/:id/login', (req, res) => {
                                 .cookie('ss', AuthToken, {sameSite: "none", secure: true, httpOnly: true, domain: 'queueupnext.com'})
                                 .cookie('rs', VToken, {sameSite: "none", secure: true, domain: 'queueupnext.com'})
                                 .send([result[0].first_name, tokenpref])
-                        }  
                         }
-
-                })} 
-                else {
-                //No valid user found
-                    if (!result[0]) {
-                        res.send({message: 'No Auth'})
                     }
+            })}
+            else {
+            //No valid user found
+                if (!result[0]) {
+                    res.send({message: 'No Auth'})
                 }
-            };   
-        });
+            }
+        };
+    });
 });
 
 module.exports = router
